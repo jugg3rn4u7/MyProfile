a05db05d6256bda4c80fcdb3b32fd17213a2f44a
shrikant <shriku4u27@gmail.com>
2016-08-23 17:53:17 -0500

    minor changes

--- mail.py
+++ mail.py
@@ -9,22 +9,24 @@ ALLOWED_ORIGINS = "*"
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}})
 
+
 @app.route("/send-mail", methods=["POST"])
+@cross_origin()
 def send_mail():
-	try:
-		json_data = request.get_json(force=True)
-		subject = MIMEText(json_data["subject"])
-		content = MIMEText(json_data["content"])
-		to = "admin@shrikantkakaraparthi.tech"
-		s = smtplib.SMTP('localhost')
-		s.sendmail(subject, to, content)
-		s.quit()
-	    return jsonify(result='ok')
-	except Exception as e:
-		e = sys.exc_info()[0]
-		print('error %s' % e)
-		print('error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-		return jsonify(result='error')
+    try:
+        json_data = request.get_json(force=True)
+        subject = MIMEText(json_data["subject"])
+        content = MIMEText(json_data["content"])
+        to = "admin@shrikantkakaraparthi.tech"
+        s = smtplib.SMTP('localhost')
+        s.sendmail(subject, to, content)
+        s.quit()
+        return jsonify(result='ok')
+    except Exception as e:
+        e = sys.exc_info()[0]
+        print('error %s' % e)
+        print('error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+        return jsonify(result='error')
 
 if __name__ == "__main__":
-	app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT)
